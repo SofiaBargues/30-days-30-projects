a42080b02efd7cc5473971661d0ba132a5e7592a
Sofia Bargues <barguessofia@gmail.com>
2024-02-04 16:50:19 +0000

    fin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
 import Image from "next/image";
 
+const DAYS = [
+  "1",
+  "2, 3 and 4",
+  "5, 6 and 7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+  "24",
+  "25",
+  "26",
+  "27",
+];
+
 const YOUTUBE_PLAYLIST_ITEMS_API =
   "https://www.googleapis.com/youtube/v3/playlistItems";
 const PLAYLIST_ID = "PLrVXEY3WfL-2AZjD_X_03OqpzGLrySOsv";
@@ -10,38 +36,21 @@ export default async function Home() {
   );
   const data = await res.json();
 
-  console.log("data", data);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="fixed lg:text-2xl left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           30 Days 30 Projects
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className="fixed lg:text-xl bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          Project based learning challenge
         </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"></div>
 
-      <ul className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        {data.items.map((item: any) => {
+      <ul className="m-10 gap-6 mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+        {data.items.map((item: any, index: number) => {
           const { id, snippet = {} } = item;
           const { title, thumbnails = {}, resourceId } = snippet;
           const { medium = {} } = thumbnails;
@@ -50,18 +59,22 @@ export default async function Home() {
               key={id}
               className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             >
-              <a href={`https://www.youtube.com/watch?v=${resourceId.videoId}`}>
-                <h2 className={`mb-3 text-2xl font-semibold`}>{title} </h2>
-                <img
+              <a
+                className="flex flex-col items-center gap-4"
+                href={`https://www.youtube.com/watch?v=${resourceId.videoId}`}
+              >
+                <h2 className={` text-2xl font-semibold`}>Day {DAYS[index]}</h2>
+                <Image
+                  className="rounded-lg"
                   width={medium.width}
                   height={medium.height}
                   src={medium.url}
-                ></img>
+                  alt={title}
+                ></Image>
                 <p
-                  className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}
+                  className={`w-full text-center text-sm opacity-50 text-balance`}
                 >
-                  Instantly deploy your Next.js site to a shareable URL with
-                  Vercel.
+                  {title}
                 </p>
               </a>
             </li>
