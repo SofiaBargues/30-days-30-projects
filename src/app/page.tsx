0db4ae88ce916d0ce4c24d4bae194d4e0de545bf
Sofia Bargues <barguessofia@gmail.com>
2024-02-04 17:07:38 +0000

    fin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,15 @@ export default async function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"></div>
+      <div
+        className="relative flex place-items-center before:absolute before:h-[300px] 
+      before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial
+       before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 
+       after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-rose-00
+        after:via-rose-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent
+         before:dark:to-pink-300 before:dark:opacity-10 after:dark:from-purple-400 after:dark:via-[#40036b] after:dark:opacity-40 
+         before:lg:h-[360px] z-[-1]"
+      ></div>
 
       <ul className="m-10 gap-6 mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
         {data.items.map((item: any, index: number) => {
