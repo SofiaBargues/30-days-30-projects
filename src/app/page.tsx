287fec63a0d1ccc5663adb2aa0c46a592cc3f6c0
Sofia Bargues <barguessofia@gmail.com>
2024-02-04 15:24:53 +0000

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 import Image from "next/image";
 
-export default function Home() {
+const YOUTUBE_PLAYLIST_ITEMS_API =
+  "https://www.googleapis.com/youtube/v3/playlistItems";
+const PLAYLIST_ID = "PLrVXEY3WfL-2AZjD_X_03OqpzGLrySOsv";
+export default async function Home() {
+  //llamada a la api
+  const res = await fetch(
+    `${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&playlistId=${PLAYLIST_ID}&maxResults=30&key=${process.env.YOUTUBE_API_KEY}`
+  );
+  const data = await res.json();
+
+  console.log("data", data);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+          30 Days 30 Projects
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
