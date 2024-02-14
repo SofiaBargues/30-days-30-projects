a98063bbd093170738bca05a2ae62fe43fbcaffd
Sofia Bargues <barguessofia@gmail.com>
2024-02-14 16:27:41 +0000

    e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Create Next App",
+  title: "30 Days 30 Projects",
   description: "30 days 30 projects",
 };
 
